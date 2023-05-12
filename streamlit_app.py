316750be91f9ed194ca4f0a32d9d959c0ad455f5
tuvshoo97 <101947872+tuvshoo97@users.noreply.github.com>
2023-05-12 19:11:13 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ model_path = Path("export.pkl")
 
 if not model_path.exists():
     with st.spinner("Downloading model... this may take awhile! \n Don't stop it!"):
-        url = 'https://drive.google.com/uc?id=1gJNYV3KB_oeS7scI9lpQIfSuj-Lb9Og0'
+        url = 'https://drive.google.com/uc?id=1cs7nWO-XIQBWqLnTjzuG_lOkEnXwyrpd'
         output = 'export.pkl'
         gdown.download(url, output, quiet=False)
     learn = load_learner('export.pkl')
