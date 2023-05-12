301d9d968f8048332403e9066be2e0eea99fc645
tuvshoo97 <101947872+tuvshoo97@users.noreply.github.com>
2023-05-12 22:19:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,10 @@ if not os.path.isfile(xml_file_path):
         gdown.download(url, xml_file_path, quiet=False)
 
 # Load the Haar Cascade Classifier for face detection
-face_cascade = cv2.CascadeClassifier(xml_file_path)
-
+try:
+    face_cascade = cv2.CascadeClassifier(xml_file_path)
+except Exception:
+    st.write("Error loading cascade classifiers")
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
 account_sid = os.environ['TWILIO_ACCOUNT_SID'] = 'ACdabf91e1a09e6a08159e78171d6639c2'
@@ -48,8 +50,7 @@ class AgeDetector(VideoTransformerBase):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         # Perform face detection using the Haar Cascade Classifier
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-
+        faces = face_cascade.detectMultiScale(image=gray, scaleFactor=1.3, minNeighbors=5)
         # Iterate over the detected faces
         for (x, y, w, h) in faces:
             # Draw a rectangle around each detected face
@@ -64,6 +65,7 @@ class AgeDetector(VideoTransformerBase):
             # Display the predicted age on the frame
             age_text = "Age: {}".format(round(age, 0))
             cv2.putText(frame, age_text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+            
 
         return frame
 
@@ -77,7 +79,7 @@ def main():
     This app was created as a project for the Deep Learning course at LETU Mongolia American University. Have fun exploring the world of age detection with live video!""")
 
     # Configure the Streamlit WebRTC component
-    webrtc_ctx = webrtc_streamer(key="example", rtc_configuration={"iceServers": token.ice_servers},
+    webrtc_ctx = webrtc_streamer(key="example",mode=WebRtcMode.SENDRECV ,rtc_configuration={"iceServers": token.ice_servers},
                                  video_processor_factory=AgeDetector)
 
 if __name__ == "__main__":
