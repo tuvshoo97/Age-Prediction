6c76d0caa0c78c68a9f91e45a56fd1f0ca086e3d
tuvshoo97 <101947872+tuvshoo97@users.noreply.github.com>
2023-05-12 21:05:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,8 @@ face_cascade = cv2.CascadeClassifier(xml_file_path)
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = "ACdabf91e1a09e6a08159e78171d6639c2"
-auth_token  = "7d290cbdef32d78db7ed34b9a8ace5f4"
+account_sid = os.environ['ACdabf91e1a09e6a08159e78171d6639c2']
+auth_token  = os.environ['7d290cbdef32d78db7ed34b9a8ace5f4']
 
 client = Client(account_sid, auth_token)
 
